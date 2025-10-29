398d548fae66e4fa61ba156d01d32add733e4c8b
HARSH-FOUJDAR <harshbtp738@gmail.com>
2025-10-29 15:24:41 +0530

    update

--- Add-Cart/src/components/Header.jsx
+++ Add-Cart/src/components/Header.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaCartPlus } from "react-icons/fa";
 import { useSelector } from "react-redux";
-import Cartitem from "./CartModal";
 const Header = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [totalQuantity, setTotalQuantity] = useState(0);
